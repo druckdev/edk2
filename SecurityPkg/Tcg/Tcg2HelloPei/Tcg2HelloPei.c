88c515d68e16dcd88e099754eab9249d28945832
Julian Prein <druckdev@protonmail.com>
2023-05-17 14:02:26 +0200

    Tcg2HelloPei: Try to get AmiTree or TreeHashPpis

--- SecurityPkg/Tcg/Tcg2HelloPei/Tcg2HelloPei.c
+++ SecurityPkg/Tcg/Tcg2HelloPei/Tcg2HelloPei.c
@@ -6,11 +6,62 @@
 #include <Library/HobLib.h>
 
 
-
-
-
 extern EFI_GUID gTestHobGuid;
 extern EFI_GUID gAmiTreePpiGuid;
+extern EFI_GUID gTrEE_HashLogExtendPpiGuid;
+
+void* hob = NULL;
+void* end = NULL;
+
+
+EFI_STATUS
+EFIAPI
+PpiNotifyCallback (
+    IN EFI_PEI_SERVICES          **PeiServices,
+    IN EFI_PEI_NOTIFY_DESCRIPTOR *NotifyDescriptor,
+    IN VOID                      *Ppi
+    )
+{
+    if (hob + 5 < end) {
+        *((UINT8*)hob++) = 'x';
+        *((UINT8*)hob++) = 1;
+        *((UINT32*)hob)  = (UINT32)Ppi;
+        hob += 4;
+    }
+    return EFI_SUCCESS;
+}
+
+void
+place_EOHOB(UINT8* hob)
+{
+    hob[0] = 'E';
+    hob[1] = 'O';
+    hob[2] = 'H';
+    hob[3] = 'O';
+    hob[4] = 'B';
+}
+
+void
+LocateOrNotify(EFI_GUID* guid) {
+    EFI_STATUS Status;
+    void* ppi;
+
+    Status = PeiServicesLocatePpi(&guid, 0, NULL, &ppi);
+    if (!Status) {
+        *((UINT32*)hob) = (UINT32)ppi;
+    } else {
+        *((UINT32*)hob) = (UINT32)Status;
+
+        EFI_PEI_NOTIFY_DESCRIPTOR notify = {
+            (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
+            guid,
+            PpiNotifyCallback
+        };
+        PeiServicesNotifyPpi(&notify);
+    }
+    hob += 4;
+}
+
 
 EFI_STATUS
 EFIAPI
@@ -19,15 +70,20 @@ TPMHelloEntryPoint(
         IN CONST EFI_PEI_SERVICES    **PeiServices
 ) {
     // https://edk2-docs.gitbook.io/edk-ii-module-writer-s-guide/7_pre-efi_initialization_modules/76_communicate_with_dxe_modules
-    char test_str[] = "s\x0EThis is a testabc";
-    void* test_hob = BuildGuidHob(&gTestHobGuid, sizeof(test_str));
+    EFI_STATUS Status;
+    void* ppi = NULL;
 
-    if (test_hob == NULL)
+    hob = BuildGuidHob(&gTestHobGuid, 24);
+    if (!hob)
         return EFI_OUT_OF_RESOURCES;
+    end = hob + 24;
+    place_EOHOB(hob + 19);
 
-    CopyMem(test_hob, test_str, sizeof(test_str));
+    *((UINT8*)hob++) = 'x';
+    *((UINT8*)hob++) = 2;
 
-    // PeiServicesLocatePpi(&gAmiTreePpiGuid, 0, NULL, (VOID**)&AmiTreePpi);
+    LocateOrNotify(&gAmiTreePpiGuid);
+    LocateOrNotify(&gTrEE_HashLogExtendPpiGuid);
 
     return EFI_SUCCESS;
 }
