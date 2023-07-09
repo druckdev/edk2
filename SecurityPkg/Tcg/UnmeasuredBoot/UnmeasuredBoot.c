e81839cfdb823d16541b43c3e48dc25de766dda2
Julian Prein <druckdev@protonmail.com>
2023-07-09 20:32:02 +0200

    UnmeasuredBoot: Remove unused code
    UnmeasuredBoot should only install the fake POST_CODE event

--- SecurityPkg/Tcg/UnmeasuredBoot/UnmeasuredBoot.c
+++ SecurityPkg/Tcg/UnmeasuredBoot/UnmeasuredBoot.c
@@ -2,70 +2,17 @@
 
 #include <IndustryStandard/Tpm20.h>
 #include <IndustryStandard/UefiTcgPlatform.h>
-#include <Protocol/TrEEProtocol.h>
 #include <Library/BaseMemoryLib.h>
-#include <Library/HobLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/PeiServicesLib.h>
 
-
-// before EOHOB
-#define InstallCount ((UINT32*)(end - 5 - 4))
-#define TreeInstallCount ((UINT32*)(end - 5 - 8))
-
-
-extern EFI_GUID gTestHobGuid;
 extern EFI_GUID gAmiTreePpiGuid;
 extern EFI_GUID gTrEE_HashLogExtendPpiGuid;
-extern EFI_GUID gTcgTestPpiGuid;
-extern EFI_GUID gUnmeasuredRockPpiGuid;
-
-EFI_STATUS EFIAPI PpiNotifyCallback(IN EFI_PEI_SERVICES **PeiServices, IN EFI_PEI_NOTIFY_DESCRIPTOR *NotifyDescriptor, IN VOID *Ppi);
-
-EFI_PEI_NOTIFY_DESCRIPTOR AmiTreePpiDesc = {
-    (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-    &gAmiTreePpiGuid,
-    PpiNotifyCallback
-};
-EFI_PEI_NOTIFY_DESCRIPTOR TrEE_HashLogExtendPpiDesc = {
-    (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-    &gTrEE_HashLogExtendPpiGuid,
-    PpiNotifyCallback
-};
-EFI_PEI_NOTIFY_DESCRIPTOR TcgTestPpiDesc = {
-    (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-    &gTcgTestPpiGuid,
-    PpiNotifyCallback
-};
-EFI_PEI_NOTIFY_DESCRIPTOR UnmeasuredRockNotifyDesc = {
-    (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-    &gUnmeasuredRockPpiGuid,
-    PpiNotifyCallback
-};
-
-EFI_PEI_PPI_DESCRIPTOR UmeasuredRockPpiDesc = {
-    (EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-    &gUnmeasuredRockPpiGuid,
-    &gUnmeasuredRockPpiGuid
-};
 
 #pragma pack (1)
 
-struct AmiTreePpi {
-    void * get_capability_maybe;
-    void * get_eventlog_maybe;
-    EFI_STATUS (*hashlog_extend_maybe)(void*, UINTN, UINTN, void*, UINTN, UINTN, UINTN, void*);
-    void * submit_command_maybe;
-};
-
 struct AmiHashLogExtendPpi {
     EFI_STATUS (*_HashLogExtendEvent)(const EFI_PEI_SERVICES**, void*, UINTN, UINTN, UINTN, UINTN, void*, void*);
-};
-
-struct TrEE_EVENT {
-    UINT32 Size;
-    TrEE_EVENT_HEADER Header;
-    UINT8 Event[];
 };
 
 struct AmiHashLogEvent {
@@ -78,124 +25,16 @@ struct AmiHashLogEvent {
 
 #pragma pack ()
 
-struct TrEE_EVENT event = {
-    0x22,
-    {
-        0xE,
-        1,
-        0,
-        EV_S_CRTM_VERSION
-    },
-    { 0xCA, 0xFE, 0xBE, 0xEF, 0xCA, 0xFE, 0xBE, 0xEF, 0xCA, 0xFE, 0xBE, 0xEF, 0xCA, 0xFE, 0xBE, 0xEF }
-};
-
-EFI_PEI_INSTALL_PPI RealInstallPpi;
-
-void* hob = NULL;
-void* end = NULL;
-
-
-EFI_STATUS
-EFIAPI
-PpiNotifyCallback(IN EFI_PEI_SERVICES **PeiServices,
-                  IN EFI_PEI_NOTIFY_DESCRIPTOR *NotifyDescriptor,
-                  IN VOID *Ppi)
-{
-    if (hob + 2 < end) {
-        *((UINT8*)hob++) = 'S';
-        *((UINT8*)hob++) = 1;
-        *((UINT8*)hob++) = EFI_SUCCESS & 0xFF;
-    }
-    if (hob + 9 < end) {
-        *((UINT8*)hob++) = 'x';
-        *((UINT8*)hob++) = 2;
-        *((UINT32*)hob) = NotifyDescriptor->Guid->Data1;
-        hob += 4;
-        *((UINT32*)hob) = 0xFFFFFFFF & (UINTN)Ppi;
-        hob += 4;
-    }
-    return EFI_SUCCESS;
-}
-
-void
-place_EOHOB(UINT8* hob)
-{
-    hob[0] = 'E';
-    hob[1] = 'O';
-    hob[2] = 'H';
-    hob[3] = 'O';
-    hob[4] = 'B';
-}
-
-EFI_STATUS
-LocateOrNotify(EFI_PEI_NOTIFY_DESCRIPTOR* notify, void** ppi)
-{
-    EFI_STATUS Status;
-
-    void* throwaway_ppi;
-    ppi = ppi ? ppi : &throwaway_ppi;
-
-    Status = PeiServicesLocatePpi(notify->Guid, 0, NULL, ppi);
-    if (EFI_SUCCESS == Status) {
-        *((UINT8*)hob++) = 'S';
-        *((UINT8*)hob++) = 1;
-        *((UINT8*)hob++) = EFI_SUCCESS & 0xFF;
-        *((UINT8*)hob++) = 'x';
-        *((UINT8*)hob++) = 2;
-        *((UINT32*)hob) = notify->Guid->Data1;
-        hob += 4;
-        *((UINT32*)hob) = 0xFFFFFFFF & (UINTN)(*ppi);
-        hob += 4;
-    } else {
-        *((UINT8*)hob++) = 'S';
-        *((UINT8*)hob++) = 1;
-        *((UINT8*)hob++) = Status & 0xFF;
-
-        if (EFI_SUCCESS != (Status = PeiServicesNotifyPpi(notify))) {
-            *((UINT8*)hob++) = 'S';
-            *((UINT8*)hob++) = 1;
-            *((UINT8*)hob++) = Status & 0xFF;
-        }
-    }
-
-    return Status;
-}
-
-
-EFI_STATUS
-EFIAPI
-HookedInstallPpi(IN CONST EFI_PEI_SERVICES** PeiServices,
-                 IN CONST EFI_PEI_PPI_DESCRIPTOR *PpiList)
-{
-    (*InstallCount)++;
-
-    if (PpiList && PpiList->Guid && PpiList->Guid->Data1 == gAmiTreePpiGuid.Data1)
-        (*TreeInstallCount)++;
-
-    return RealInstallPpi(PeiServices, PpiList);
-}
-
 EFI_STATUS
 EFIAPI
 EntryPoint(IN EFI_PEI_FILE_HANDLE FileHandle,
            IN CONST EFI_PEI_SERVICES **PeiServices)
 {
-    // https://edk2-docs.gitbook.io/edk-ii-module-writer-s-guide/7_pre-efi_initialization_modules/76_communicate_with_dxe_modules
-    UINTN len = 64;
-    hob = BuildGuidHob(&gTestHobGuid, len);
-    if (!hob) {
-        if ((hob = BuildGuidHob(&gTestHobGuid, 5)))
-            place_EOHOB(hob);
-        return EFI_OUT_OF_RESOURCES;
-    }
-    end = hob + len;
-    place_EOHOB(end - 5);
-
     struct AmiHashLogExtendPpi* HashLogPpi;
-    struct AmiTreePpi* TreePpi;
+    void* TreePpi;
 
-    if (EFI_SUCCESS == LocateOrNotify(&TrEE_HashLogExtendPpiDesc, (VOID**)&HashLogPpi)
-            && EFI_SUCCESS == LocateOrNotify(&AmiTreePpiDesc, (VOID**)&TreePpi)) {
+    if (!PeiServicesLocatePpi(&gTrEE_HashLogExtendPpiGuid, 0, NULL, (VOID**)&HashLogPpi)
+        && !PeiServicesLocatePpi(&gAmiTreePpiGuid, 0, NULL, &TreePpi)) {
 
         UINT32 extra[4] = {0xFF92F000, 0, 0x4D1000, 0};
         struct AmiHashLogEvent event = {
@@ -211,10 +50,7 @@ EntryPoint(IN EFI_PEI_FILE_HANDLE FileHandle,
             .EventSize = 0x10
         };
 
-        *((UINT8*)hob++) = 'S';
-        *((UINT8*)hob++) = 1;
-        *((UINT8*)hob++) = 0xFF & \
-                           (*(HashLogPpi->_HashLogExtendEvent))(PeiServices, TreePpi, 0, 0, 0, 0, &event, &extra);
+        (*(HashLogPpi->_HashLogExtendEvent))(PeiServices, TreePpi, 0, 0, 0, 0, &event, &extra);
     }
 
     return EFI_SUCCESS;
